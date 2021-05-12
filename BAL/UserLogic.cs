ed92f12866871bda789bf1161fec8feed5245704
emad <imad_elberdaoui@outlook.com>
2021-05-12 18:48:40 +0000

    Add project files.

---
+++ BAL/UserLogic.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Configuration;
+using LoginSystemTest.DataAccess;
+
+namespace LoginSystemTest.BAL
+{
+    public class UserLogic
+    {
+        private readonly loginTestEntities entities;
+
+        public UserLogic()
+        {
+            entities = new loginTestEntities();
+        }
+
+        public List<User> GetUsers()
+        {
+            //var users = entities.Users.Select(u => new { u.FirstName, u.LastName, u.UserName }).ToList();
+            //var list = new List<object>();
+            //foreach (var user in users) list.Add(user);
+            ////return list.ToList();
+            ////return entities.Users.ToList();
+            var usr = (from u in entities.Users
+                select u).ToList();
+            var result = from u in usr
+                select new User()
+                {
+                    FirstName = u.FirstName,
+                    LastName = u.LastName,
+                    UserName = u.UserName
+                };
+            return result.ToList();
+        }
+
+        public async Task<User> CreateUserAsync(User user)
+        {
+            entities.Users.Add(user);
+            await entities.SaveChangesAsync();
+            return user;
+        }
+
+        public List<User> Login(string userName, string password)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(password))
+                {
+                    return null;
+                }
+                var result = entities.Users.FirstOrDefault(u => u.UserName == userName && u.Password == password);
+                if (result.UserName != userName || result.Password != password)
+                {
+                    // return new CustomError(statusCode: 404, Uri("UserService.asmx"));
+                    return null;
+                }
+                return GetUsers();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw e;
+                //e.Message("something wrong");
+            }
+            
+            
+        }
+    }
+}
---
+++ UserService.asmx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Services;
+using LoginSystemTest.BAL;
+using LoginSystemTest.DataAccess;
+
+namespace LoginSystemTest
+{
+    /// <summary>
+    /// Summary description for UserService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class UserService : System.Web.Services.WebService
+    {
+        private readonly UserLogic userLogic;
+
+        public UserService()
+        {
+            userLogic = new UserLogic();
+        }
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public List<User> GetAll()
+        {
+            return userLogic.GetUsers();
+        }
+
+        [WebMethod]
+        public async Task<User> CreateUser(int id, string firstName, string lastName, string userName, string password)
+        {
+            var user = new User()
+            {
+                Id = id,
+                FirstName = firstName,
+                LastName = lastName,
+                UserName = userName,
+                Password = password
+            };
+            await userLogic.CreateUserAsync(user);
+            return user;
+        }
+
+        [WebMethod]
+        public List<User> Login(string username, string password)
+        {
+            
+            var use =  userLogic.Login(username, password);
+            return use;
+        }
+    }
+}
